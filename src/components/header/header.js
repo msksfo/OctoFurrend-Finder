9532ec5bd6167b2626e7f92b1562895e5e7bc38c
Tia Esguerra <msksfo@gmail.com>
2020-09-06 13:53:40 -0700

    refactor: delete inspectocat image becuase Github does not allow it to be used as app icon. Replace with new cat detective image.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,14 @@
 import PropTypes from "prop-types"
 import React from "react"
 import headerStyles from "../header/header.module.css"
-import octocatDetective from "../../images/octocatDetective.png"
+import catDetective from "../../images/catDetective.png"
 
 const Header = ({ siteTitle }) => (
   <header>
     <img
       className={headerStyles.inspectocat}
-      src={octocatDetective}
-      alt="Inspectocat"
+      src={catDetective}
+      alt="Cat Detective"
     />
     <h1 className={headerStyles.title}>OctoFurrend Finder</h1>
   </header>
