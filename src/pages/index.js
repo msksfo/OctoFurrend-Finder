77dd2da50b9c2d745c414a1ddfb6696b75c5a2be
Tia Esguerra <msksfo@gmail.com>
2020-09-06 15:11:55 -0700

    fix: add Helmet component to include site meta information. This fixes failed audit of missing title and html lang attribute.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import indexStyles from "../styles/index.module.css"
 
 import Layout from "../components/layout/layout"
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi furrrrends!</h1>
-    <p> I'm Inspectocat. Let's find some amazing Github users, shall we?</p>
-  </Layout>
+  <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>OctoFurrend Finder</title>
+      <html lang="en"></html>
+    </Helmet>
+    <Layout>
+      <h1>Hi furrrrends!</h1>
+      <p> I'm Inspectocat. Let's find some amazing Github users, shall we?</p>
+    </Layout>
+  </div>
 )
 
 export default IndexPage
